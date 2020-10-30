23f462e24d65ad327d3551741315b06c7435eb86
cgamboasanabria <cesar.gamboasanabria@ucr.ac.cr>
2020-10-30 14:52:03 -0600

    update correlations

--- R/popstudy.R
+++ R/popstudy.R
@@ -14,7 +14,7 @@
 #' @importFrom dplyr select filter group_by mutate summarise arrange mutate_all row_number left_join right_join rename desc mutate_if ungroup contains
 #' @importFrom ggplot2 ggplot theme_classic scale_x_continuous geom_density aes geom_segment scale_y_continuous geom_text theme element_text element_blank margin ggtitle labs dup_axis element_line geom_area geom_density geom_vline
 #' @importFrom tidyr gather pivot_wider separate
-#' @importFrom stats complete.cases density quantile sd na.omit coef lm
+#' @importFrom stats complete.cases density quantile sd na.omit coef lm xtabs
 #' @importFrom demography read.demogdata extract.years set.upperage lifetable lca
 #' @importFrom forecast forecast
 #' @importFrom moments kurtosis skewness
@@ -22,4 +22,6 @@
 #' @importFrom grid unit
 #' @importFrom stackoverflow match.call.defaults
 #' @importFrom utils combn
+#' @importFrom rcompanion wilcoxonRG
+#' @importFrom DescTools Lambda
 NULL
