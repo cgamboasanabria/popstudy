f0e70e9d32977a0e318809501f8f690f08b9520d
cgamboasanabria <cesar.gamboasanabria@ucr.ac.cr>
2021-07-01 09:56:08 -0600

    required_packages function

---
+++ R/required_packages.R
@@ -0,0 +1,21 @@
+#' required_packages
+#'
+#' Install/load the required packages.
+#'
+#' @param ... packages names.
+#'
+#' @examples
+#'
+#' required_packages(tidyr, dplyr, ggplot2)
+#'
+#' @author Cesar Gamboa-Sanabria
+#'
+#' @export
+required_packages <- function(...){
+    paq <- as.list(match.call())[-1]
+    paq <- sapply(paq, paste)
+    paq.nuevo <- paq[!(paq %in% installed.packages()[, "Package"])]
+    if(length(paq.nuevo))
+        install.packages(paq.nuevo, dependencies = TRUE, quiet = TRUE)
+    suppressPackageStartupMessages(sapply(paq, require, character.only = TRUE)) #
+}
